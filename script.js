b07c71ccd4739ef44cc99df2b88108558c894676
chetan71 <91386497+chetan71@users.noreply.github.com>
2025-01-30 16:03:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let student = {
+	name : Mahesh,
+	age : 18
+	
+};
+function getKeys(obj) {
+	let keys = object.keys(obj);
+	return keys;
+};
+let arr = getKeys(student);
+console.log(arr);
